cf4a4d7f1c1f0484521d37470ad6fd4c072fee68
chiweliterem <itprince4real39@gmail.com>
2025-02-19 21:49:22 +0100

    Added a directory and a file for managing colors.

---
+++ lib/colors/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//Base color
+const kWhite = Color(0xffFFFFFF);
+const kBlack = Color(0xFF000000);
+
+//AppBar Color
+const kBackgroundColor = Color(0xffFAF5F2);
+
+//Shape colors
+const kCircle = Color(0xffFB9E11);
+const kRectangle = Color(0xffFEF8F3);
+const kFAB = Color(0xff2B2B2B);
+
+//Text colors
+const kAppBarText = Color(0xffAAA4A0);
+const kBodyText = Color(0xff27241F);
+const kNumberText = Color(0xffA79782);
+const kNumberText1 = Color(0xffFEF6F5);
