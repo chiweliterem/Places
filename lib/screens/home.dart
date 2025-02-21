1a658980ee4d833039f7f812046f38f90d8c57b9
chiweliterem <itprince4real39@gmail.com>
2025-02-21 12:50:59 +0100

    Back button disable on the appbar.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -18,6 +18,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: kBackgroundColor,
         title: AppTitleWidget(title: "Saint Petersburg"),
         actions: [
