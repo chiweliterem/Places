1a45640860cf45c00b798c618aa719a047c63882
chiweliterem <itprince4real39@gmail.com>
2025-02-22 10:12:53 +0100

    Number animation completed on both circle and square widgets.

--- lib/reusable/shape_circle.dart
+++ lib/reusable/shape_circle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:places/colors/colors.dart';
 import 'package:places/reusable/label.dart';
 
@@ -28,13 +29,31 @@ class ShapeCircle extends StatelessWidget {
               ).textTheme.titleSmall!.copyWith(color: kWhite),
             ),
             SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-            CustomLabel(
-              name: secondLabel,
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                fontSize: 40.0,
-                color: kWhite,
-                fontWeight: FontWeight.w700,
-              ),
+
+            //Implementing animation
+            TweenAnimationBuilder<int>(
+              tween: IntTween(begin: 1, end: 1038),
+              duration: 1500.ms, // Adjust animation duration
+              builder: (_, value, __) {
+                return CustomLabel(
+                      name:
+                          value.toString() == "1038"
+                              ? secondLabel
+                              : value.toString(),
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        fontSize: 40.0,
+                        color: kWhite,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    )
+                    .animate() // Apply flutter_animate effects
+                    .fadeIn(duration: 300.ms) // Fade in the number smoothly
+                    .scale(
+                      begin: Offset(0.5, 2.0),
+                      end: Offset(1.0, 1.0),
+                      duration: 300.ms,
+                    ); // Subtle scale effect
+              },
             ),
             CustomLabel(
               name: thirdLabel,
